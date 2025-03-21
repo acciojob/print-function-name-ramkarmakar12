a2c4284bb13478c35985a2b8800a19f83398ff21
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-21 08:34:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+function functionName() {
+    // ✅ Display the function's name using arguments.callee.name
+    alert(arguments.callee.name);
+}
+
+// ✅ Call the function to test
+functionName();
